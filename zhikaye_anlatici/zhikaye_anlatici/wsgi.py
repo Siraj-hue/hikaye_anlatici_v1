47b50fac3cc3ef8cc37945bad095417de3e2ba4f
Harun <harunustaoglu181@gmail.com>
2025-11-11 16:26:07 +0300

    Initial Commit

---
+++ zhikaye_anlatici/zhikaye_anlatici/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for zhikaye_anlatici project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zhikaye_anlatici.settings')
+
+application = get_wsgi_application()
